c91221f7cbea323c5af3312a15d6c202a399b6d8
David Reiss <dreiss@facebook.com>
2008-03-27 21:22:28 -0700

    First steps toward de-virtualizing TTransport.
    - Pull readAll out of TTransport to be a global templatized helper function.
    - Create a TVirtualTransport as a bridge between the virtual TTransport
      and nonvirtual implementations.
    
    Chad Walters wrote the first version of this patch.  I (David Reiss) just
    pulled out the simplest parts.  I left the TTransportUtils part out for now.
    The calls to the underlying transports are going to be relatively rare, and
    probably quite expensive (a system call), so de-virtualizing them will probably
    not be that big of a performance win.

---
+++ lib/cpp/src/transport/TVirtualTransport.h
@@ -0,0 +1,82 @@
+// Copyright (c) 2006- Facebook
+// Distributed under the Thrift Software License
+//
+// See accompanying file LICENSE or visit the Thrift site at:
+// http://developers.facebook.com/thrift/
+
+#ifndef _THRIFT_TRANSPORT_TVIRTUALTRANSPORT_H_
+#define _THRIFT_TRANSPORT_TVIRTUALTRANSPORT_H_ 1
+
+#include <TTransport.h>
+
+namespace facebook { namespace thrift { namespace transport {
+
+/**
+ * TODO
+ *
+ * @author Chad Walters <chad@powerst.com>
+ */
+template <class Transport_>
+  class TVirtualTransport : public TTransport {
+ public:
+  /**
+   * Virtual deconstructor.
+   */
+  virtual ~TVirtualTransport() {}
+
+  virtual bool isOpen_virt() {
+    return ((Transport_*)this)->isOpen();
+  }
+
+  virtual bool peek_virt() {
+    return ((Transport_*)this)->peek();
+  }
+
+  virtual void open_virt() {
+    ((Transport_*)this)->open();
+  }
+
+  virtual void close_virt() {
+    ((Transport_*)this)->close();
+  }
+
+  virtual uint32_t read_virt(uint8_t* buf, uint32_t len) {
+    return ((Transport_*)this)->read(buf, len);    
+  }
+
+  virtual uint32_t readAll_virt(uint8_t* buf, uint32_t len) {
+    return ((Transport_*)this)->readAll(buf, len);
+  }
+
+  virtual void readEnd_virt() {
+    return ((Transport_*)this)->readEnd();
+  }
+
+  virtual void write_virt(const uint8_t* buf, uint32_t len) {
+    ((Transport_*)this)->write(buf, len);
+  }
+
+  virtual void writeEnd_virt() {
+    ((Transport_*)this)->writeEnd();
+  }
+
+  virtual void flush_virt() {
+    ((Transport_*)this)->flush();
+  }
+
+  virtual const uint8_t* borrow_virt(uint8_t* buf, uint32_t* len) {
+    ((Transport_*)this)->borrow(buf, len);    
+  }
+
+  virtual void consume(uint32_t len) {
+    ((Transport_*)this)->consume(len);    
+  }
+
+ protected:
+
+  TVirtualTransport() {}
+};
+
+}}} // facebook::thrift::transport
+
+#endif // #ifndef _THRIFT_TRANSPORT_TVIRTUALTRANSPORT_H_
