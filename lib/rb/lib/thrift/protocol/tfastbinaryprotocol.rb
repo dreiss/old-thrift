4e3dc2ddb0fb52d2e7bb6be207a0113434eaa5af
Kevin Clark <kevin.clark@gmail.com>
2008-04-22 19:33:50 -0700

    rb: Add compatability requirements to TFastBinaryProtocol's ruby file

--- lib/rb/lib/thrift/protocol/tfastbinaryprotocol.rb
+++ lib/rb/lib/thrift/protocol/tfastbinaryprotocol.rb
@@ -1,1 +1,11 @@
 require File.join(File.dirname(__FILE__), '../../../ext/tfastbinaryprotocol')
+
+=begin
+The only change required for a transport to support TFastBinaryProtocol is to implement 3 methods:
+  * string_buffer, which returns all unconsumed data as a string
+  * refill_buffer(size), which refills the internal string buffer and
+  returns the value of the string, returning atleast size bytes
+  * consume!(size), which removes size bytes from the front of the buffer
+
+See TMemoryBuffer and TBufferedTransport for examples.
+=end
