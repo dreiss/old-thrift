11dcc1bfde1b5b93f87233a6158c50cd5223c178
Kevin Clark <kevin.clark@gmail.com>
2008-04-26 02:06:43 -0700

    rb: Cleanup TBinaryProtocolAccelerated documentation

--- lib/rb/lib/thrift/protocol/tbinaryprotocolaccelerated.rb
+++ lib/rb/lib/thrift/protocol/tbinaryprotocolaccelerated.rb
@@ -3,7 +3,8 @@ require File.join(File.dirname(__FILE__), '../../../ext/tbinaryprotocolaccelerat
 
 =begin
 The only change required for a transport to support TFastBinaryProtocol is to implement 2 methods:
-  * borrow(size), which returns atleast _size_ bytes from the transport
+  * borrow(size), which takes an optional argument and returns atleast _size_ bytes from the transport, 
+                  or the default buffer size if no argument is given
   * consume!(size), which removes size bytes from the front of the buffer
 
 See TMemoryBuffer and TBufferedTransport for examples.
