07d926a6cc3b55d9df9e5d3ff734eba86e66f37c
David Reiss <dreiss@facebook.com>
2008-05-09 19:38:28 -0700

    Inline a lot of TBinaryProtocolT methods.

--- lib/cpp/src/protocol/TBinaryProtocol.h
+++ lib/cpp/src/protocol/TBinaryProtocol.h
@@ -76,109 +76,109 @@ class TBinaryProtocolT : public TVirtualProtocol< TBinaryProtocolT<Transport_> >
    * Writing functions.
    */
 
-  virtual uint32_t writeMessageBegin(const std::string& name,
-                                     const TMessageType messageType,
-                                     const int32_t seqid);
+  /*ol*/ uint32_t writeMessageBegin(const std::string& name,
+                                    const TMessageType messageType,
+                                    const int32_t seqid);
 
-  virtual uint32_t writeMessageEnd();
+  /*ol*/ uint32_t writeMessageEnd();
 
 
-  uint32_t writeStructBegin(const char* name);
+  inline uint32_t writeStructBegin(const char* name);
 
-  uint32_t writeStructEnd();
+  inline uint32_t writeStructEnd();
 
-  uint32_t writeFieldBegin(const char* name,
-                           const TType fieldType,
-                           const int16_t fieldId);
+  inline uint32_t writeFieldBegin(const char* name,
+                                  const TType fieldType,
+                                  const int16_t fieldId);
 
-  uint32_t writeFieldEnd();
+  inline uint32_t writeFieldEnd();
 
-  uint32_t writeFieldStop();
+  inline uint32_t writeFieldStop();
 
-  uint32_t writeMapBegin(const TType keyType,
-                         const TType valType,
-                         const uint32_t size);
+  inline uint32_t writeMapBegin(const TType keyType,
+                                const TType valType,
+                                const uint32_t size);
 
-  uint32_t writeMapEnd();
+  inline uint32_t writeMapEnd();
 
-  uint32_t writeListBegin(const TType elemType,
-                          const uint32_t size);
+  inline uint32_t writeListBegin(const TType elemType,
+                                 const uint32_t size);
 
-  uint32_t writeListEnd();
+  inline uint32_t writeListEnd();
 
-  uint32_t writeSetBegin(const TType elemType,
-                         const uint32_t size);
+  inline uint32_t writeSetBegin(const TType elemType,
+                                const uint32_t size);
 
-  uint32_t writeSetEnd();
+  inline uint32_t writeSetEnd();
 
-  uint32_t writeBool(const bool value);
+  inline uint32_t writeBool(const bool value);
 
-  uint32_t writeByte(const int8_t byte);
+  inline uint32_t writeByte(const int8_t byte);
 
-  uint32_t writeI16(const int16_t i16);
+  inline uint32_t writeI16(const int16_t i16);
 
-  uint32_t writeI32(const int32_t i32);
+  inline uint32_t writeI32(const int32_t i32);
 
-  uint32_t writeI64(const int64_t i64);
+  inline uint32_t writeI64(const int64_t i64);
 
-  uint32_t writeDouble(const double dub);
+  inline uint32_t writeDouble(const double dub);
 
-  uint32_t writeString(const std::string& str);
+  inline uint32_t writeString(const std::string& str);
 
-  uint32_t writeBinary(const std::string& str);
+  inline uint32_t writeBinary(const std::string& str);
 
   /**
    * Reading functions
    */
 
 
-  uint32_t readMessageBegin(std::string& name,
-                            TMessageType& messageType,
-                            int32_t& seqid);
+  /*ol*/ uint32_t readMessageBegin(std::string& name,
+                                   TMessageType& messageType,
+                                   int32_t& seqid);
 
-  uint32_t readMessageEnd();
+  /*ol*/ uint32_t readMessageEnd();
 
-  uint32_t readStructBegin(std::string& name);
+  inline uint32_t readStructBegin(std::string& name);
 
-  uint32_t readStructEnd();
+  inline uint32_t readStructEnd();
 
-  uint32_t readFieldBegin(std::string& name,
-                          TType& fieldType,
-                          int16_t& fieldId);
+  inline uint32_t readFieldBegin(std::string& name,
+                                 TType& fieldType,
+                                 int16_t& fieldId);
 
-  uint32_t readFieldEnd();
+  inline uint32_t readFieldEnd();
 
-  uint32_t readMapBegin(TType& keyType,
-                        TType& valType,
-                        uint32_t& size);
+  inline uint32_t readMapBegin(TType& keyType,
+                               TType& valType,
+                               uint32_t& size);
 
-  uint32_t readMapEnd();
+  inline uint32_t readMapEnd();
 
-  uint32_t readListBegin(TType& elemType,
-                         uint32_t& size);
+  inline uint32_t readListBegin(TType& elemType,
+                                uint32_t& size);
 
-  uint32_t readListEnd();
+  inline uint32_t readListEnd();
 
-  uint32_t readSetBegin(TType& elemType,
-                        uint32_t& size);
+  inline uint32_t readSetBegin(TType& elemType,
+                               uint32_t& size);
 
-  uint32_t readSetEnd();
+  inline uint32_t readSetEnd();
 
-  uint32_t readBool(bool& value);
+  inline uint32_t readBool(bool& value);
 
-  uint32_t readByte(int8_t& byte);
+  inline uint32_t readByte(int8_t& byte);
 
-  uint32_t readI16(int16_t& i16);
+  inline uint32_t readI16(int16_t& i16);
 
-  uint32_t readI32(int32_t& i32);
+  inline uint32_t readI32(int32_t& i32);
 
-  uint32_t readI64(int64_t& i64);
+  inline uint32_t readI64(int64_t& i64);
 
-  uint32_t readDouble(double& dub);
+  inline uint32_t readDouble(double& dub);
 
-  uint32_t readString(std::string& str);
+  inline uint32_t readString(std::string& str);
 
-  uint32_t readBinary(std::string& str);
+  inline uint32_t readBinary(std::string& str);
 
  protected:
   uint32_t readStringBody(std::string& str, int32_t sz);
