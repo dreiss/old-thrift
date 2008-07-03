3e6987953f017b6c45f97b69cba0fe87ab0f0486
Alexander Shigin <shigin@rambler-co.ru>
2008-07-03 20:12:05 +0400

    * add docstrings; * makes pylint happy.

--- lib/py/src/server/TNonblockingServer.py
+++ lib/py/src/server/TNonblockingServer.py
@@ -1,3 +1,10 @@
+"""Implementation of non-blocking server.
+
+The main idea of the server is reciving and sending requests
+only from main thread.
+
+It also makes thread pool server in tasks terms, not connections.
+"""
 import threading
 import socket
 import Queue
@@ -5,17 +12,17 @@ import select
 import struct
 import logging
 
-from thrift.Thrift import TProcessor
 from thrift.transport import TTransport
 from thrift.protocol import TBinaryProtocol
-from thrift.server import TServer
 
 class Slave(threading.Thread):
+    """Worker is a small helper to process incoming connection."""
     def __init__(self, queue):
         threading.Thread.__init__(self)
         self.queue = queue
 
     def run(self):
+        """Process queries from task queue, stop if processor is None."""
         while True:
             try:
                 processor, iprot, oprot, otrans, callback = self.queue.get()
@@ -23,7 +30,7 @@ class Slave(threading.Thread):
                     break
                 processor.process(iprot, oprot)
                 callback(True, otrans.getvalue())
-            except Exception, err:
+            except Exception:
                 logging.exception("Exception while processing request")
                 callback(False, '')
 
@@ -34,6 +41,7 @@ SEND_ANSWER = 3
 CLOSED = 4
 
 def locked(func):
+    "Decorator which locks self.lock."
     def nested(self, *args, **kwargs):
         self.lock.acquire()
         try:
@@ -43,6 +51,7 @@ def locked(func):
     return nested
 
 def socket_exception(func):
+    "Decorator close object on socket.error."
     def read(self, *args, **kwargs):
         try:
             return func(self, *args, **kwargs)
@@ -51,8 +60,19 @@ def socket_exception(func):
     return read
 
 class Connection:
-    def __init__(self, socket, wake_up):
-        self.socket = socket
+    """Basic class is represented connection.
+    
+    It can be in state:
+        WAIT_LEN --- connection is reading request len.
+        WAIT_MESSAGE --- connection is reading request.
+        WAIT_PROCESS --- connection has just read whole request and 
+            waits for call ready routine.
+        SEND_ANSWER --- connection is sending answer string (including length
+            of answer).
+        CLOSED --- socket was closed and connection should be deleted.
+    """
+    def __init__(self, new_socket, wake_up):
+        self.socket = new_socket
         self.socket.setblocking(False)
         self.status = WAIT_LEN
         self.len = ''
@@ -61,6 +81,10 @@ class Connection:
         self.wake_up = wake_up
 
     def _read_len(self):
+        """Reads length of request.
+        
+        It's really paranoic routine and it may be replaced by 
+        self.socket.recv(4)."""
         read = self.socket.recv(4 - len(self.len))
         if len(read) == 0:
             self.close()
@@ -69,7 +93,8 @@ class Connection:
         if len(self.len) == 4:
             self.len, = struct.unpack('!i', self.len)
             if self.len < 0:
-                logging.error("negative size, it seems client doesn't use FramedTransport")
+                logging.error("negative frame size, it seems client"\
+                    " doesn't use FramedTransport")
                 self.close()
                 return
             self.message = ''
@@ -77,6 +102,7 @@ class Connection:
 
     @socket_exception
     def read(self):
+        """Reads data from stream and switch state."""
         if self.status == WAIT_LEN:
             self._read_len()
         elif self.status == WAIT_MESSAGE:
@@ -92,6 +118,7 @@ class Connection:
 
     @socket_exception
     def write(self):
+        """Writes data from socket and switch state."""
         assert self.status == SEND_ANSWER
         sent = self.socket.send(self.message)
         if sent == len(self.message):
@@ -100,14 +127,26 @@ class Connection:
             self.message = self.message[sent:]
 
     @locked
-    def ready(self, all_ok, buffer):
+    def ready(self, all_ok, message):
+        """Callback function for switching state and waking up main thread.
+        
+        This function is the only function witch can be called asynchronous.
+        
+        The ready can switch Connection to three states:
+            WAIT_LEN if request was async.
+            SEND_ANSWER if request was processed in normal way.
+            CLOSED if request throws unexpected exception.
+        
+        The one wakes up main thread.
+        """
+        assert self.status == WAIT_PROCESS
         if not all_ok:
             self.close()
             self.wake_up()
             return
         self.len = ''
-        self.message = struct.pack('!i', len(buffer)) + buffer
-        if len(buffer) == 0:
+        self.message = struct.pack('!i', len(message)) + message
+        if len(message) == 0:
             # it was async request, do not write answer
             self.status = WAIT_LEN
         else:
@@ -116,25 +155,31 @@ class Connection:
 
     @locked
     def is_writeable(self):
+        "Returns True if connection should be added to write list of select."
         return self.status == SEND_ANSWER
 
     # it's not necessary, but...
     @locked
     def is_readable(self):
+        "Returns True if connection should be added to read list of select."
         return self.status in (WAIT_LEN, WAIT_MESSAGE)
 
     @locked
     def is_closed(self):
+        "Returns True if connection is closed."
         return self.status == CLOSED
 
     def fileno(self):
+        "Returns the file descriptor of the associated socket."
         return self.socket.fileno()
 
     def close(self):
+        "Closes connection"
         self.status = CLOSED
         self.socket.close()
 
 class TNonblockingServer:
+    """Non-blocking server."""
     def __init__(self, processor, lsocket, protocol=None, threads=10):
         self.processor = processor
         self.socket = lsocket
@@ -145,6 +190,7 @@ class TNonblockingServer:
         self._read, self._write = socket.socketpair()
 
     def prepare(self):
+        """Prepares server for serve requests."""
         self.socket.listen()
         for _ in xrange(self.threads):
             thread = Slave(self.tasks)
@@ -152,9 +198,19 @@ class TNonblockingServer:
             thread.start()
 
     def wake_up(self):
+        """Wake up main thread.
+        
+        The server usualy waits in select call in we should terminate one.
+        The simplest way is using socketpair.
+        
+        Select always wait to read from the first socket of socketpair.
+        
+        In this case, we can just write anything to the second socket from
+        socketpair."""
         self._write.send('1')
 
     def _select(self):
+        """Does select on open connections."""
         readable = [self.socket.handle.fileno(), self._read.fileno()]
         writable = []
         for i, connection in self.clients.items():
@@ -169,10 +225,16 @@ class TNonblockingServer:
     select = _select
 
     def handle(self):
-        r, w, x = self.select()
-        for readable in r:
+        """Handle requests.
+       
+        WARNING! You must call prepare BEFORE calling handle.
+
+        TODO: checks if server was prepared."""
+        rset, wset, xset = self.select()
+        for readable in rset:
             if readable == self._read.fileno():
-                self._read.recv(1024) # don't care i just need to clean readable flag
+                # don't care i just need to clean readable flag
+                self._read.recv(1024) 
             elif readable == self.socket.handle.fileno():
                 client = self.socket.accept().handle
                 self.clients[client.fileno()] = Connection(client, self.wake_up)
@@ -184,21 +246,24 @@ class TNonblockingServer:
                     otransport = TTransport.TMemoryBuffer()
                     iprot = self.protocol.getProtocol(itransport)
                     oprot = self.protocol.getProtocol(otransport)
-                    self.tasks.put([self.processor, iprot, oprot, otransport, connection.ready])
-        for writeable in w:
+                    self.tasks.put([self.processor, iprot, oprot, 
+                                    otransport, connection.ready])
+        for writeable in wset:
             self.clients[writeable].write()
-        for oob in x:
+        for oob in xset:
             for i, connection in self.clients.items():
                 if connection.fileno() == oob:
                     connection.close()
                     del self.clients[i]
 
     def close(self):
+        """Closes the server."""
         for _ in xrange(self.threads):
             self.tasks.put([None, None, None, None, None])
         self.socket.close()
         
     def serve(self):
+        """Serve forever."""
         self.prepare()
         while True:
             self.handle()
