b72c72913c7ef7572a6c6ecefe772a2bd4bd5685
Alexander Shigin <shigin@rambler-co.ru>
2008-07-26 11:35:29 +0400

    check for ECONNRESET only for freebsd

--- lib/cpp/src/transport/TSocket.cpp
+++ lib/cpp/src/transport/TSocket.cpp
@@ -96,7 +96,8 @@ bool TSocket::peek() {
   int r = recv(socket_, &buf, 1, MSG_PEEK);
   if (r == -1) {
     int errno_copy = errno;
-    /* shigin: dirty hack
+    #ifdef __FreeBSD__
+    /* shigin:
      * freebsd returns -1 and ECONNRESET if socket was closed by 
      * the other side
      */
@@ -105,6 +106,7 @@ bool TSocket::peek() {
       close();
       return false;
     }
+    #endif
     GlobalOutput.perror("TSocket::peek() recv() " + getSocketInfo(), errno_copy);
     throw TTransportException(TTransportException::UNKNOWN, "recv()", errno_copy);
   }
