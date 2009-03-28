d85d7200ac0962578a62ffc5f7381935fe669178
Alexander Shigin <shigin@rambler-co.ru>
2009-03-28 17:34:49 +0300

    start implementation of compact protocol

---
+++ lib/py/src/protocol/TCompactProtocol.py
@@ -0,0 +1,115 @@
+from TProtocol import *
+from struct import pack, unpack
+
+__all__ = ['TCompactProtocol']
+
+CLEAR = 0
+BOOL_STATE = 1
+WAIT_VALUE = 2
+
+def writer(func):
+  def nested(self, *args, **kwargs):
+    assert self.__state == WAIT_VALUE
+    try:
+      return func(self, *args, **kwargs)
+    finally:
+      self.__state = CLEAR
+  return nested
+
+def makeZigZag(n, bits):
+  return (n << 1) ^ (n >> (bits - 1))
+
+TYPES = {True: 0x01, False: 0x02}
+class TCompactProtocol(TProtocolBase):
+  "Compact implementation of the Thrift protocol driver."
+
+  __state = CLEAR
+  __last = None
+  __id = None
+  def __init__(self, trans):
+    TProtocolBase.__init__(self, trans)
+
+  def __writeFieldHeader(self, type, id):
+    try:
+      if self.__last and id > self.__last:
+        delta = id - self.__last
+        if delta < 16:
+          return self.writeByte(delta << 4 | type)
+      self.__writeByte(type)
+      self.__writeI16(id)
+      return 3
+    finally:
+      self.__last = id
+
+  def writeFieldBegin(self, name, type, id):
+    assert state == CLEAR
+    if type == TType.BOOL:
+      self.__state = BOOL_STATE
+      self.__id = id
+    else:
+      self.__state = WAIT_VALUE
+      self.__writeFieldHeader(type, id)
+
+  def __writeByte(self, byte):
+    self.trans.write(pack('!b', byte))
+
+  def __writeI16(self, i16):
+    self.writeVarint(makeZigZag(i16, 16))
+
+  def __writeVarint(self, n):
+    out = []
+    while True:
+      if n & 0x80:
+        out.append(n)
+      else:
+        out.append(n & 0xff)
+        n = n >> 7
+    self.trans.write(''.join(map(chr, out)))
+
+  def __writeSize(self, i32):
+    if i32 > 0x7fff:
+      raise TException("thrift can't handle strings longer 0x7FFF")
+    self.writeVarint(i32)
+
+  def writeCollectionBegin(self, etype, size):
+    assert self.WAIT_VALUE
+    if size <= 14:
+      self.__writeByte(size << 4 | TYPES[etype])
+    else:
+      self.__writeByte(0xf0 | TYPES[etype])
+      self.__writeSize(size)
+  writeSetBegin = writeCollectionBegin
+  writeListBegin = writeCollectionBegin
+
+  def writeMapBegin(self, ktype, vtype, size):
+    assert self.WAIT_VALUE
+    if size == 0:
+      self.__writeByte(0)
+    else:
+      self.__writeSize(size)
+      self.__writeByte(TYPES[ktype] << 4 | TYPES[vtype])
+
+  def writeBool(self, bool):
+    assert self.__state == BOOL_STATE
+    self.__writeFieldHeader(types[bool], self.__id)
+    self.__state = CLEAR
+
+  writeByte = writer(__writeByte)
+  writeI16 = writer(__writeI16)
+
+  @writer
+  def writeI32(self, i32):
+    self.__writeVarint(makeZigZag(i32, 32))
+
+  @writer
+  def writeI64(self, i64):
+    self.__writeVarint(makeZigZag(i64, 64))
+
+  @writer
+  def writeDouble(self, dub):
+    self.trans.write(pack('!d', dub))
+
+  @writer
+  def writeString(self, s):
+    self.__writeSize(len(s))
+    self.trans.write(str)
